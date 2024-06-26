2b98a554e88282e6eb6cbaefda77f7bbaa2afadb
Dhiraj Alate <dhirajalate92@gmail.com>
2024-06-27 01:38:10 +0530

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,19 +1,21 @@
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
 
-public class GameFrame extends JFrame{
-
-    GamePanel panel;
-
+public class GameFrame extends JFrame implements Runnable{
     GameFrame(){
-        panel = new GamePanel();
+        GamePanel panel = new GamePanel();
         this.add(panel);
-        this.setTitle("Pong Game");
-        this.setResizable(false);
-        this.setBackground(new Color(0x9DC171));
+        this.setTitle("Ping-Pong Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
+        this.setResizable(false);
         this.setVisible(true);
+        this.setBackground(new Color(0x9DC171));
+        this.pack();
         this.setLocationRelativeTo(null);
+    }
+
+    @Override
+    public void run() {
+
     }
 }

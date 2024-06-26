45f032001b681497dedfb9431d179b165d77ee2c
Dhiraj Alate <dhirajalate92@gmail.com>
2024-06-27 01:39:10 +0530

    Update Paddle.java

--- Paddle.java
+++ Paddle.java
@@ -1,68 +1,78 @@
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.KeyEvent;
 
-public class Paddle extends Rectangle{
-
+public class Paddle extends Rectangle {
     int id;
     int yVelocity;
-    int speed = 10;
-
-    Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id){
-        super(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
+    int speed=10;
+    Paddle(int x,int y,int width,int height,int id){
+        super(x,y,width,height);
         this.id=id;
     }
+    public void move(){
+        y=y+yVelocity;
+    }
+    public void setYDirection(int yDirection){
+        this.yVelocity = yDirection;
+    }
+    public void draw(Graphics g){
+      if (id==1)
+          g.setColor(Color.RED);
+      else
+          g.setColor(Color.ORANGE);
 
-    public void keyPressed(KeyEvent e) {
-        switch(id) {
-            case 1:
-                if(e.getKeyCode()==KeyEvent.VK_W) {
-                    setYDirection(-speed);
+      g.fillRect(x,y,width,height);
+    }
+    public void keyPressed(KeyEvent e){
+        //System.out.println("key pressed");
+         switch (id){
+             case 1: {
+                 if (e.getKeyCode() == KeyEvent.VK_W) {
+                    // System.out.println("1up");
+                     setYDirection(-speed);
+                 }
+                 if (e.getKeyCode() == KeyEvent.VK_S) {
+                     setYDirection(speed);
+                 }
+                 move();
+                 break;
+             }
+             case  2: {
+                 if (e.getKeyCode() == KeyEvent.VK_UP) {
+                     setYDirection(-speed);
+                 }
+                 if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                     setYDirection(speed);
+                 }
+                 move();
+                 break;
+             }
+         }
+    }
+    public void keyReleased(KeyEvent e){
+        //System.out.println("key released");
+        switch (id){
+            case 1: {
+                if (e.getKeyCode() == KeyEvent.VK_W) {
+                    //System.out.println("1up");
+                    setYDirection(0);
                 }
-                if(e.getKeyCode()==KeyEvent.VK_S) {
-                    setYDirection(speed);
+                if (e.getKeyCode() == KeyEvent.VK_S) {
+                    setYDirection(0);
                 }
+                move();
                 break;
-            case 2:
-                if(e.getKeyCode()==KeyEvent.VK_UP) {
-                    setYDirection(-speed);
+            }
+            case  2: {
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    setYDirection(0);
                 }
-                if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-                    setYDirection(speed);
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    setYDirection(0);
                 }
+                move();
                 break;
+            }
         }
-    }
-    public void keyReleased(KeyEvent e) {
-        switch(id) {
-            case 1:
-                if(e.getKeyCode()==KeyEvent.VK_W) {
-                    setYDirection(0);
-                }
-                if(e.getKeyCode()==KeyEvent.VK_S) {
-                    setYDirection(0);
-                }
-                break;
-            case 2:
-                if(e.getKeyCode()==KeyEvent.VK_UP) {
-                    setYDirection(0);
-                }
-                if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-                    setYDirection(0);
-                }
-                break;
-        }
-    }
-    public void setYDirection(int yDirection) {
-        yVelocity = yDirection;
-    }
-    public void move() {
-        y= y + yVelocity;
-    }
-    public void draw(Graphics g) {
-        if(id==1)
-            g.setColor(new Color(175, 175, 31));
-        else
-            g.setColor(new Color(224, 39, 39));
-        g.fillRect(x, y, width, height);
     }
 }

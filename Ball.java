a5682d43e129dd0a415c9cece50db400e19d3b4a
Dhiraj Alate <dhirajalate92@gmail.com>
2024-06-27 01:39:54 +0530

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,40 +1,43 @@
 import java.awt.*;
-import java.util.*;
+import java.util.Random;
 
-public class Ball extends Rectangle{
-
+public class Ball extends Rectangle {
     Random random;
     int xVelocity;
     int yVelocity;
-    int initialSpeed = 2;
+    int ballSpeed=2;
 
-    Ball(int x, int y, int width, int height){
-        super(x,y,width,height);
-        random = new Random();
-        int randomXDirection = random.nextInt(2);
-        if(randomXDirection == 0)
-            randomXDirection--;
-        setXDirection(randomXDirection*initialSpeed);
-
-        int randomYDirection = random.nextInt(2);
-        if(randomYDirection == 0)
-            randomYDirection--;
-        setYDirection(randomYDirection*initialSpeed);
-
+    Ball(int x,int y,int width,int height){
+      super(x,y,width,height);
+      random = new Random();
+     //for x axis
+      int randomXDirection = random.nextInt(2);
+      if(randomXDirection==0){
+          //move ball to the left .
+         randomXDirection--;
+      }
+      setXDirection(randomXDirection*ballSpeed);
+      //for y axis
+      int randomYDirection = random.nextInt(2);
+      if(randomYDirection==0){
+          randomYDirection--;
+      }
+      setYDirection(randomYDirection*ballSpeed);
+    }
+    public void move(){
+        x=x+xVelocity;
+        y=y+yVelocity;
+    }
+    public void setXDirection(int xDirection){
+         xVelocity=xDirection;
+    }
+    public void setYDirection(int yDirection){
+      yVelocity=yDirection;
+    }
+    public void draw(Graphics g){
+         g.setColor(Color.WHITE);
+         g.fillOval(x,y,width,height);
     }
 
-    public void setXDirection(int randomXDirection) {
-        xVelocity = randomXDirection;
-    }
-    public void setYDirection(int randomYDirection) {
-        yVelocity = randomYDirection;
-    }
-    public void move() {
-        x += xVelocity;
-        y += yVelocity;
-    }
-    public void draw(Graphics g) {
-        g.setColor(Color.white);
-        g.fillOval(x, y, height, width);
-    }
+
 }
